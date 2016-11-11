1c557e86e18c98f5806cc016a4cdd362e8e84da7
Master Yoda <michael.kjeldsen@vaimo.com>
2016-11-11 14:09:55 +0100

    dynamic release

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+REL=$(lsb_release -sc)
 ./autogen.sh
 ./configure --prefix=/usr --with-apxs=/usr/bin/apxs2 --with-apr=/usr/bin/apr-config --enable-pcre-match-limit=no --enable-pcre-match-limit-recursion=no --enable-htaccess-config --disable-mlogc --with-yajl=/tmp
-checkinstall --maintainer="michael.kjeldsen@vaimo.com" --pkgversion="2.x" --pkgrelease="trusty" --requires="libapr1,libaprutil1,libcurl3-gnutls,libxml2,libpcre3,libc6"
+checkinstall --maintainer="michael.kjeldsen@vaimo.com" --pkgversion="2.x" --pkgrelease="$REL" --requires="libapr1,libaprutil1,libcurl3-gnutls,libxml2,libpcre3,libc6"
